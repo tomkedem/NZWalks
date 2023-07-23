c00a40c5899db10a25689a4c5b2c9f145c812a7a
TomerK <tompha1@hotmail.com>
2023-07-23 21:55:52 +0300

    seeting up Authentication

--- NZWalks.API/Controllers/RegionController.cs
+++ NZWalks.API/Controllers/RegionController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace NZWalks.API.Controllers
     // https://localhost:portnumber/api/regions
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionController : ControllerBase
     {
         private readonly IRegionRepository _regionRepository;
